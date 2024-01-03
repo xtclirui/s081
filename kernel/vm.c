65949c3e083efe0dacd7d53eca45fdb146b4b394
lirui <821342264@qq.com>
2024-01-03 19:18:11 +0800

    lab3-3

--- kernel/vm.c
+++ kernel/vm.c
@@ -387,7 +387,7 @@ copyin(pagetable_t pagetable, char *dst, uint64 srcva, uint64 len)
     va0 = PGROUNDDOWN(srcva);
     pa0 = walkaddr(pagetable, va0);
     if(pa0 == 0)
-      return -1;ss
+      return -1;
     n = PGSIZE - (srcva - va0);
     if(n > len)
       n = len;
